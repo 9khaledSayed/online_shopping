e91ca01813f11d3ade03fc8f9163fc45e1d77990
Hossamghoneim <hossamghoneim11@gmail.com>
2020-06-05 04:13:34 +0200

    City ar lang

--- resources/views/dashboard/cities/create.blade.php
+++ resources/views/dashboard/cities/create.blade.php
@@ -17,18 +17,18 @@
             <div class="panel panel-card margin-b-30">
                 <!-- Start .panel -->
                 <div class="panel-heading">
-                    <h4 class="panel-title"> Add New City</h4>
+                    <h4 class="panel-title">{{__('New City')}}</h4>
                 </div>
 
                 <div class="panel-body">
                     <form role="form" method="POST" action="{{route('dashboard.cities.store')}}">
                         @csrf
                         <div class="form-group @error('name') has-error @enderror">  <!-- form-group has-error -->
-                            <label>Name</label>
+                            <label>{{__('Name')}}</label>
                             <input
                                 type="text"
                                 name="name"
-                                placeholder="Enter City Name"
+                                placeholder="{{__('Enter City Name')}}"
                                 class="form-control"
                                 value="{{old('name')}}">
                             @error('name')
@@ -36,7 +36,7 @@
                             @enderror
                         </div>
 
-                        <button class="btn btn-sm btn-primary pull-right m-t-n-xs" type="submit"><strong>submit</strong></button>
+                        <button class="btn btn-sm btn-primary pull-right m-t-n-xs" type="submit"><strong>{{__('Confirm')}}</strong></button>
                     </form>
                 </div>
             </div>
